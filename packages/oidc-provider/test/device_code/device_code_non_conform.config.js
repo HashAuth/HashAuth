18463032759a043f9f34d3981f2897aa1d71beac
mcfeelio <owenjmcneil@gmail.com>
2024-12-10 11:59:04 -0500

    Revert fork of oidc-provider

--- packages/oidc-provider/test/device_code/device_code_non_conform.config.js
+++
@@ -1,9 +0,0 @@
-import cloneDeep from 'lodash/cloneDeep.js';
-
-import config from './device_code.config.js';
-
-const setup = cloneDeep(config);
-
-setup.config.conformIdTokenClaims = false;
-
-export default setup;
--- packages/oidc-provider/test/id_token_claims/non_conform.config.js
+++
@@ -1,9 +0,0 @@
-import cloneDeep from 'lodash/cloneDeep.js';
-
-import config from './conform.config.js';
-
-const setup = cloneDeep(config);
-
-setup.config.conformIdTokenClaims = false;
-
-export default setup;
